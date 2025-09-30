fa47c82d1217d4a32f4c0b7b6b0111cfe74d57bd
NINADKHOPADE <147337850+NINADKHOPADE@users.noreply.github.com>
2025-09-30 17:42:22 +0530

    new update

---
+++ FRONTEND/frontone/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'jsx-a11y/img-redundant-alt': 'warn',
+    'eqeqeq': 'warn'
+  }
+};
