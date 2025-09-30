0a921e71b3d84a994bad3ac25e305605f3a13954
NINADKHOPADE <147337850+NINADKHOPADE@users.noreply.github.com>
2025-09-30 17:54:20 +0530

    Delete FRONTEND directory

--- FRONTEND/frontone/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['react-app'],
-  rules: {
-    'no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    'jsx-a11y/img-redundant-alt': 'warn',
-    'eqeqeq': 'warn'
-  }
-};
